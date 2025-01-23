e5b65425084dc12d71ab46123407bcf44152e79c
Mustafayazilim63 <162355104+Mustafayazilim63@users.noreply.github.com>
2025-01-23 15:13:15 +0300

    Delete makaleapp directory

--- makaleapp/makale/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Article,Bugün,afiş,daha_yenilik,daha_sorular
-
-admin.site.register(Category)
-admin.site.register(Article)
-admin.site.register(Bugün)
-admin.site.register(afiş)
-admin.site.register(daha_yenilik)
-admin.site.register(daha_sorular)
